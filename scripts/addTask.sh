2b2088b413d10f424197b2a3731bed2ffeae2fe9
jtviegas <jtviegas.js@google.com>
2017-11-09 00:07:11 +0000

    ...

--- scripts/addTask.sh
+++ scripts/addTask.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" -d '{"name":"dothingz","host":"198.162.1.1","status":0,"start":0,"end":0}' http://apaa.mybluemix.net/api/tasks/add
+curl -H "Content-Type: application/json" -d '{"name":"dothingz","host":"198.162.1.1","status":0,"start":12.6,"end":34.7}' http://apaa.mybluemix.net/api/tasks/add
